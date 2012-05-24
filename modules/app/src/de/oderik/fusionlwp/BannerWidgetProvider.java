d7b5c989b8a9d556bb0a1ef305639207302714da
Oderik <Oderik@c5ef991c-3c6a-15ae-8198-784ab691e4e9>
2012-05-24 08:18:20 +0000

    Bilder-Widgets

---
+++ modules/app/src/de/oderik/fusionlwp/BannerWidgetProvider.java
@@ -0,0 +1,11 @@
+package de.oderik.fusionlwp;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created: 24.05.12
+ *
+ * @author Oderik
+ */
+public class BannerWidgetProvider extends AppWidgetProvider {
+}
