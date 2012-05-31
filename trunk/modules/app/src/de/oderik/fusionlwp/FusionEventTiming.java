dbe8af13853304fbce11c629078f36f16920ac0a
oderik <oderik@c5ef991c-3c6a-15ae-8198-784ab691e4e9>
2012-05-31 19:16:02 +0000

    Neues Eventtiming (IN PROGRESS!)

--- trunk/modules/app/src/de/oderik/fusionlwp/FusionEventTiming.java
+++ trunk/modules/app/src/de/oderik/fusionlwp/FusionEventTiming.java
@@ -1,7 +1,6 @@
 package de.oderik.fusionlwp;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 /**
@@ -10,16 +9,11 @@ import java.util.TimeZone;
  * @author Oderik
  */
 public class FusionEventTiming {
-  private final static Calendar FUSION_CALENDAR = GregorianCalendar.getInstance();
 
-  static {
-    FUSION_CALENDAR.clear();
-    FUSION_CALENDAR.setTimeZone(TimeZone.getTimeZone("UTC"));
-    FUSION_CALENDAR.set(Calendar.YEAR, 2012);
-    FUSION_CALENDAR.set(Calendar.MONTH, Calendar.JUNE);
-    FUSION_CALENDAR.set(Calendar.DAY_OF_MONTH, 28);
-    FUSION_CALENDAR.set(Calendar.HOUR_OF_DAY, 16);
-    FUSION_CALENDAR.set(Calendar.MINUTE, 0);
+  private static FusionEventTiming INSTANCE = new FusionEventTiming();
+
+  public static FusionEventTiming get() {
+    return INSTANCE;
   }
 
   public final static long SECOND = 1000;
@@ -27,17 +21,33 @@ public class FusionEventTiming {
   public final static long HOUR   = 60 * MINUTE;
   public final static long DAY    = 24 * HOUR;
 
-  public static long timeToFusion() {
-    final Calendar now = Calendar.getInstance();
-    return FUSION_CALENDAR.getTimeInMillis() - now.getTimeInMillis();
+  public enum Phase {
+    BEFORE (SECOND),
+    DURING (MINUTE);
+
+    private final long interval;
+
+    Phase(final long interval) {
+      this.interval = interval;
+    }
+
+    public long getInterval() {
+      return interval;
+    }
   }
 
+  private static String[] TIMETABLE_DAY_OF_WEEK_NAMES = {"", "SO", "MO", "DI", "MI", "DO", "FR", "SA"};
 
-  public static long timeToNextSecond() {
+  private Phase phase = Phase.BEFORE;
+
+  private FusionEventTiming() {
+  }
+
+  public long timeToNextSecond() {
     return SECOND - (System.currentTimeMillis() % SECOND);
   }
 
-  public static long nextSecond() {
+  public long nextSecond() {
     return (System.currentTimeMillis() / SECOND + 1) * SECOND;
   }
 
@@ -48,5 +58,40 @@ public class FusionEventTiming {
     final long seconds = time % MINUTE / SECOND;
 
     return String.format("%02dd %02dh %02dm %02ds", days, hours, minutes, seconds);
+  }
+
+  private Calendar getFusionCalendar() {
+    final Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin"));
+    final int currentYear = calendar.get(Calendar.YEAR);
+    calendar.clear();
+    calendar.set(Calendar.YEAR, currentYear);
+    calendar.set(Calendar.MONTH, Calendar.JUNE);
+    calendar.set(Calendar.HOUR_OF_DAY, 18);
+    calendar.set(Calendar.DAY_OF_WEEK, Calendar.THURSDAY);
+    calendar.set(Calendar.DAY_OF_WEEK_IN_MONTH, -Calendar.THURSDAY);
+    return calendar;
+  }
+
+  private void ensureInFuture(final Calendar calendar) {
+    while (calendar.getTimeInMillis() < System.currentTimeMillis()) {
+      calendar.roll(Calendar.YEAR, true);
+    }
+  }
+
+  private long nextFusionStart() {
+    final Calendar calendar = getFusionCalendar();
+    ensureInFuture(calendar);
+    return calendar.getTimeInMillis();
+  }
+
+  private long nextFusionEnd() {
+    final Calendar calendar = getFusionCalendar();
+    calendar.roll(Calendar.DAY_OF_YEAR, 5);
+    ensureInFuture(calendar);
+    return calendar.getTimeInMillis();
+  }
+
+  private Phase getCurrentPhase() {
+    return nextFusionStart() < nextFusionEnd() ? Phase.BEFORE : Phase.DURING;
   }
 }
