1131852d30426c5851ab767f871b349928a0c73f
Oderik.Stern@gmail.com <Oderik.Stern@gmail.com@c5ef991c-3c6a-15ae-8198-784ab691e4e9>
2013-05-19 21:28:02 +0000

    Skalierung verfeinert

--- trunk/modules/app/src/de/oderik/fusionlwp/wallpaper/Background2013Drawable.java
+++ trunk/modules/app/src/de/oderik/fusionlwp/wallpaper/Background2013Drawable.java
@@ -5,26 +5,25 @@ import android.content.res.TypedArray;
 import android.graphics.*;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
+import de.oderik.fusionlwp.BuildConfig;
 import de.oderik.fusionlwp.R;
 import de.oderik.fusionlwp.util.BitmapFactory;
 import de.oderik.fusionlwp.util.OffsetsHolder;
+
+import static android.graphics.BitmapFactory.Options;
 
 /**
  * @author maik.riechel
  * @since 14.05.13 20:25
  */
 public class Background2013Drawable extends LiveBackgroundDrawable {
-  private static final String TAG = Background2013Drawable.class.getName();
-
   protected static final float MAX_PARALLAX_SCROLL = .3f;
-
+  private static final String TAG = Background2013Drawable.class.getName();
   private final int[] parallaxLayerResourceIds;
   private final Bitmap[] parallaxLayers;
-
   private final Paint backgroundPaint = new Paint();
-
   private final OffsetsHolder offsetsHolder = new OffsetsHolder();
-
   private final int intrinsicParallaxWidth;
   private final int intrinsicParallaxHeight;
 
@@ -47,7 +46,7 @@ public class Background2013Drawable extends LiveBackgroundDrawable {
       backgroundPaint.setShader(new BitmapShader(((BitmapDrawable) backgroundDrawable).getBitmap(), Shader.TileMode.REPEAT, Shader.TileMode.REPEAT));
     }
 
-    final android.graphics.BitmapFactory.Options options = new android.graphics.BitmapFactory.Options();
+    final Options options = new Options();
     options.inJustDecodeBounds = true;
     BitmapFactory.decodeResource(resources, R.drawable.rakete_2013, options);
     intrinsicParallaxWidth = options.outWidth;
@@ -56,17 +55,19 @@ public class Background2013Drawable extends LiveBackgroundDrawable {
 
   @Override
   public void draw(final Canvas canvas) {
-    canvas.save();
-    canvas.translate(offsetsHolder.getxPixels(), offsetsHolder.getyPixels());
-    canvas.drawPaint(backgroundPaint);
-    canvas.restore();
+    synchronized (offsetsHolder) {
+      canvas.save();
+      canvas.translate(offsetsHolder.getxPixels(), offsetsHolder.getyPixels());
+      canvas.drawPaint(backgroundPaint);
+      canvas.restore();
 
-    if (parallaxLayers != null) {
-      for (int i = 0; i < parallaxLayers.length; i++) {
-        final Bitmap layer = parallaxLayers[i];
-        final float left = calculateLayerPosition(getBounds().width(), getVirtualWidth(), layer.getWidth(), offsetsHolder.getHorizontals(), parallaxLayers.length, i);
-        final float top = calculateLayerPosition(getBounds().height(), getVirtualHeight(), layer.getHeight(), offsetsHolder.getVerticals(), parallaxLayers.length, i);
-        canvas.drawBitmap(layer, left, top, paint);
+      if (parallaxLayers != null) {
+        for (int i = 0; i < parallaxLayers.length; i++) {
+          final Bitmap layer = parallaxLayers[i];
+          final float left = calculateLayerPosition(getBounds().width(), getVirtualWidth(), layer.getWidth(), offsetsHolder.getHorizontals(), parallaxLayers.length, i);
+          final float top = calculateLayerPosition(getBounds().height(), getVirtualHeight(), layer.getHeight(), offsetsHolder.getVerticals(), parallaxLayers.length, i);
+          canvas.drawBitmap(layer, left, top, paint);
+        }
       }
     }
   }
@@ -84,18 +85,41 @@ public class Background2013Drawable extends LiveBackgroundDrawable {
 
   @Override
   protected void onBoundsChange(final Rect bounds) {
-    final android.graphics.BitmapFactory.Options options = new android.graphics.BitmapFactory.Options();
-    options.inSampleSize = Math.max(intrinsicParallaxWidth / bounds.width(), intrinsicParallaxHeight / bounds.height());
+    final int width = bounds.width();
+    final int height = bounds.height();
+
+    final Options options = new Options();
+    options.inSampleSize = Math.max(intrinsicParallaxWidth / width, intrinsicParallaxHeight / height);
+
+    final float scale = Math.min((float) options.inSampleSize * width / intrinsicParallaxWidth, (float) options.inSampleSize * height / intrinsicParallaxHeight);
+
+    if (BuildConfig.DEBUG) {
+      Log.d(TAG, "Reloading resources with sample size of " + options.inSampleSize + ", scaling " + scale);
+    }
+
+    final Matrix matrix = new Matrix();
+    matrix.setScale(scale, scale);
 
     for (int i = 0; i < parallaxLayerResourceIds.length; i++) {
+      if (parallaxLayers[i] != null) {
+        parallaxLayers[i].recycle();
+      }
       parallaxLayers[i] = BitmapFactory.decodeResource(resources, parallaxLayerResourceIds[i], options);
+
+      if (scale < 1) {
+        final Bitmap scaledLayer = Bitmap.createBitmap((int) (parallaxLayers[i].getWidth() * scale), (int) (parallaxLayers[i].getHeight() * scale), Bitmap.Config.ARGB_8888);
+        new Canvas(scaledLayer).drawBitmap(parallaxLayers[i], matrix, new Paint(Paint.FILTER_BITMAP_FLAG | Paint.ANTI_ALIAS_FLAG));
+        parallaxLayers[i].recycle();
+        parallaxLayers[i] = scaledLayer;
+      }
     }
   }
 
   @Override
   protected void onOffsetsChanged(final float xOffset, final float yOffset, final float xStep, final float yStep, final int xPixels, final int yPixels) {
-    offsetsHolder.set(xOffset, yOffset, xStep, yStep, xPixels, yPixels);
-
-    invalidateSelf();
+    synchronized (offsetsHolder) {
+      offsetsHolder.set(xOffset, yOffset, xStep, yStep, xPixels, yPixels);
+      invalidateSelf();
+    }
   }
 }
