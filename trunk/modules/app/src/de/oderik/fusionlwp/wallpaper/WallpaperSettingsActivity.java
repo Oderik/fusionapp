388340bba0f79642791afb3ed9115888066f3e2d
Oderik.Stern@gmail.com <Oderik.Stern@gmail.com@c5ef991c-3c6a-15ae-8198-784ab691e4e9>
2013-05-23 19:22:59 +0000

    APK verkleinert

--- trunk/modules/app/src/de/oderik/fusionlwp/wallpaper/WallpaperSettingsActivity.java
+++ trunk/modules/app/src/de/oderik/fusionlwp/wallpaper/WallpaperSettingsActivity.java
@@ -2,7 +2,6 @@ package de.oderik.fusionlwp.wallpaper;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
