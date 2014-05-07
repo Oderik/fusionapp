f8686611b54ad5ae857f071e913d06f379e53819
oderikstern <oderikstern@c5ef991c-3c6a-15ae-8198-784ab691e4e9>
2014-05-07 06:27:18 +0000

    Fixed event calculation. Again. D'uh.

--- modules/java/test/de/oderik/fusionlwp/event/FestivalStartEventTest.java
+++ modules/java/test/de/oderik/fusionlwp/event/FestivalStartEventTest.java
@@ -2,6 +2,8 @@ package de.oderik.fusionlwp.event;
 
 import java.util.Calendar;
 
+import static java.util.Calendar.*;
+
 /**
  * @author Maik.Riechel
  * @since 21.11.12
@@ -14,13 +16,14 @@ public class FestivalStartEventTest extends EventTest {
   }
 
   public void testGetTimestamp() throws Exception {
-    configure(calendar, 2011, Calendar.APRIL, 1, 12);
-    assertTimestamp(2011, Calendar.JUNE, 30, 18, event.getTimestamp());
+    configure(calendar, 2014, MAY, 7, 7);
+    assertTimestamp(2014, JUNE, 26, 18, event.getTimestamp());
 
-    configure(calendar, 2011, Calendar.JULY, 1, 12);
-    assertTimestamp(2012, Calendar.JUNE, 28, 18, event.getTimestamp());
+    configure(calendar, 2011, APRIL, 1, 12);
+    assertTimestamp(2011, JUNE, 30, 18, event.getTimestamp());
 
-
+    configure(calendar, 2011, JULY, 1, 12);
+    assertTimestamp(2012, JUNE, 28, 18, event.getTimestamp());
   }
 
 }
